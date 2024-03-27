528b0096da5d16f9201b4f4f01e5931f1a429540
MettaCode <mettacode@gmail.com>
2024-03-27 18:44:52 -0500

    update

--- index.js
+++ index.js
@@ -92,10 +92,18 @@ async function getVideos(sources) {
 
         const url = `${idBase}${source.id}/videos`;
 
+        // await getYoutubeVideoList(source.name, url)
+        //     .then((channelVideos) => {
+        //         console.log(`${channelVideos.length} Total Channel Videos`);
+        //         videos.push({ "channel-name": source.name, "channel-id": source.id, "channel-slug": source.slug, "channel-url": url, "channel-videos": channelVideos.length > 7 ? channelVideos.slice(0, 6) : channelVideos });
+        //         // console.log(videoList);
+        //     });
+
         await getYoutubeVideoList(source.name, url)
             .then((channelVideos) => {
-                console.log(`${channelVideos.length} Total Channel Videos`);
-                videos.push({ "channel-name": source.name, "channel-id": source.id, "channel-slug": source.slug, "channel-url": url, "channel-videos": channelVideos.length > 7 ? channelVideos.slice(0, 6) : channelVideos });
+                console.log(`[${channelVideos.length}] Matched videos for this channel`);
+                ///
+                videos.push({ "channel-name": source.name, "channel-id": source.id, "channel-slug": source.slug, "channel-url": url, "channel-videos": channelVideos.slice(0, 6) });
                 // console.log(videoList);
             });
 
