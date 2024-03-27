93b50aaea4a9f82468e5fa233e5e2423010b45ab
MettaCode <mettacode@gmail.com>
2024-03-27 18:49:46 -0500

    update

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ async function getVideos(sources) {
             .then((channelVideos) => {
                 // console.log(`[${channelVideos.length}] Matched videos for this channel`);
                 ///
-                videos.push({ "channel-name": source.name, "channel-id": source.id, "channel-slug": source.slug, "channel-url": url, "channel-videos": channelVideos.slice(0, 6) });
+                videos.push({ "channel-name": source.name, "channel-id": source.id, "channel-slug": source.slug, "channel-url": url, "channel-videos": channelVideos });
                 // console.log(videoList);
             });
 
